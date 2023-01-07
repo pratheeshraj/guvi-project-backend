07df483ddd200b7da7a8b78a8a5f3b600e592ee2
pratheeshraj <rajeshnsk5@gmail.com>
2023-01-07 17:04:12 +0530

    ok

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:3000", "https://frolicking-chimera-fd41d2.netlify.app"],
     credentials: true,
   })
 );
